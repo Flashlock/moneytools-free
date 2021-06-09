0ca5ab58aa010902a725892d458687de1cbc8546
Austin Betts <65870500+Flashlock@users.noreply.github.com>
2021-06-08 18:08:13 -0700

    Some updates and new scripts from Stackem Up

--- Object_Pooling/ObjectPooler.cs
+++ Object_Pooling/ObjectPooler.cs
@@ -1,74 +1,62 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPooler : MonoBehaviour
-{
+namespace ObjectPooling{
     /*
-     * This class instantiates any necessary objects up front
-     * to save processing power later on.
-     * There can only be 1 ObjectPooler per scene
-     * Spawned Objects likely come from this class, and 
-     * Dead Objects are likely returned to this class
-     * To "Despawn" a Pooled Object, deactivate it, maybe put it somewhere like the origin.
-     */
-
-    public static System.Exception unknownPool =
-        new System.Exception("Unknown Pool: Queue Not Found");
-    public static System.Exception emptyPool =
-        new System.Exception("The Pool is Empty, Can't Spawn Object");
-
-    public static ObjectPooler Instance { get; private set; }
-
-    [System.Serializable]
-    public class Pool
+    Generates pools of objects to pull from.
+    */
+    public class ObjectPooler : Singleton<ObjectPooler>
     {
-        public string tag;
-        public GameObject prefab;
-        public int size;
-    }
-
-    [SerializeField]
-    private Pool[] pools;
-
-    private Dictionary<string, Queue<GameObject>> poolBook;
-    private void Awake()
-    {
-        if (Instance != null)
-            Destroy(this);
-        Instance = this;
-        if (pools == null) return;
-
-        poolBook = new Dictionary<string, Queue<GameObject>>();
-        for(int i = 0; i < pools.Length; i++)
-        {
-            Queue<GameObject> poolQueue = new Queue<GameObject>();
-            for(int j = 0; j < pools[i].size; j++)
-            {
-                GameObject g = Instantiate(pools[i].prefab, Vector3.zero, Quaternion.identity);
-                g.SetActive(false);
-                poolQueue.Enqueue(g);
-            }
-            poolBook.Add(pools[i].tag, poolQueue);
+#region Serialization
+        [System.Serializable]
+        public class Pool{
+            public string tag;
+            public GameObject template;
+            public int count;
         }
-    }
 
-    private void TestContains(string tag)
-    {
-        if (!poolBook.ContainsKey(tag))
-            throw unknownPool;
-    }
+        [SerializeField]
+        private Pool[] pools;
+#endregion
 
-    public GameObject SpawnObject(string tag, Vector3 position, Quaternion rotation)
-    {
-        TestContains(tag);
+        private Dictionary<string, Queue<GameObject>> poolBook;
 
-        GameObject g = poolBook[tag].Dequeue();
-        if (g.activeInHierarchy) throw emptyPool;
+        private void Awake(){
+            poolBook = new Dictionary<string, Queue<GameObject>>();
 
-        g.transform.position = position;
-        g.transform.rotation = rotation;
-        g.SetActive(true);
-        poolBook[tag].Enqueue(g);
-        return g;
+            for(int i = 0; i < pools.Length; i++){
+                Queue<GameObject> poolQueue = new Queue<GameObject>();
+                for(int j = 0; j < pools[i].count; j++){
+                    GameObject g = Instantiate(pools[i].template, Vector3.zero, Quaternion.identity);
+                    g.SetActive(false);
+                    g.transform.SetParent(transform);
+                    poolQueue.Enqueue(g);
+                }
+                poolBook.Add(pools[i].tag, poolQueue);
+            }
+        }
+
+        /*
+        Spawns an object from the dictionary.
+        */
+        public GameObject SpawnObject(string tag, Vector3 position, Quaternion rotation, GameObject manager){
+            GameObject g = poolBook[tag].Dequeue();
+            g.transform.SetParent(null);
+            g.transform.position = position;
+            g.transform.rotation = rotation;
+            g.SetActive(true);
+            g.GetComponent<ISpawnable>().Spawn(manager);
+            return g;
+        }
+
+        /*
+        Despawns an object, returns it to the dictionary.
+        */
+        public void DespawnObject(string tag, GameObject gameObject){
+            gameObject.SetActive(false);
+            poolBook[tag].Enqueue(gameObject);
+            gameObject.transform.SetParent(transform);
+            gameObject.GetComponent<ISpawnable>().Despawn();
+        }
     }
 }
